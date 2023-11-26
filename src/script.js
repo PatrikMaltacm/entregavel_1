f88a7c203c71cf49543a41e642d9b64a08c9943f
Patrik Malta <maltapatrik3@gmail.com>
2023-11-26 14:34:13 -0300

    Calculadora finalizada

--- src/script.js
+++ src/script.js
@@ -3,6 +3,39 @@
 
 //ESCREVA SEU CÓDIGO AQUI!!
 
+function Calculadora(num1, num2, operador){
+  let resultado = ""
+  let num1r = parseInt(num1);
+  let num2r = parseInt(num2);
+
+  switch(operador){
+    case "+":
+      resultado = num1r + num2r
+      break;
+    case "-":
+      resultado = num1r - num2r
+      break;
+    case "*":
+      resultado = num1r * num2r
+      break;
+    case "/":
+      resultado = num1r / num2r
+      break;
+    case "e":
+      for( i = 0; i <= num2r - 1; i++){
+        resultado = num1r * num1r * num1r
+      }
+      break;
+  }
+  if (resultado === undefined || resultado >= 1000000){
+    resultado = "ERRO";
+  }
+
+  console.log(resultado)
+  return resultado;
+
+}
+
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
